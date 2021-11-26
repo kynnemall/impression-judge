b0fa115bf6e9d1d4ece142951d2ee57bb18c0565
Martin <59205886+kynnemall@users.noreply.github.com>
2021-11-26 18:31:22 +0000

    Working on layout

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 import streamlit as st
+
+st.set_page_config(page_title="Impersonation Judge AI Web App",
+                   page_icon="judge",
+                   layout="wide")
+st.title("Impersonation Judge")
